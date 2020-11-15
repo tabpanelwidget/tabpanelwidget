aca1ba66514de38ef01abc0c4995fe4a67d6782b
TJ Koblentz <tj.koblentz@gmail.com>
2020-11-15 08:04:41 -0800

    control when deploy-test-page workflow runs + delete examples directory

--- examples/react/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import ReactTabpanelwidget from "../../src/tabpanelwidget.react.jsx"
-
-ReactDOM.render(<ReactTabpanelwidget tabs={["a", "b", "c"]} />, document.getElementById("app"))
