f152335f0b095a420b14aa3426f1835b82b7db96
thierryk <thierry.koblentz@gmail.com>
2020-11-23 14:27:32 -0800

    Updated markup + styles

--- testpage/App.vue
+++ testpage/App.vue
@@ -4,7 +4,7 @@
     link(v-if="stylesheetHref" rel="stylesheet" :href="stylesheetHref")
     link(v-if="customStylesheetHref" rel="stylesheet" :href="customStylesheetHref")
     header
-      h1 TabpanelWidget Test Page &middot; #[a(title="TabpanelWidget.com" href="https://tabpanelwidget.com") tabpanelwidget.com]
+      h1 TabPanelWidget Test Page &middot; #[a(title="TabpanelWidget.com" href="https://tabpanelwidget.com") tabpanelwidget.com]
       //-p All the widgets below are only styled via #[strong tabpanelwidget.min.css]
       p
         input#normalizeYN.input(type="checkbox" v-model="addNormalize")
@@ -49,51 +49,48 @@
         svg(role="presentation" xmlns="http://www.w3.org/2000/svg" width="50" height="35" viewBox="0 0 640 512")
           path(fill="#fff" d="M629.657 343.598L528.971 444.284c-9.373 9.372-24.568 9.372-33.941 0L394.343 343.598c-9.373-9.373-9.373-24.569 0-33.941l10.823-10.823c9.562-9.562 25.133-9.34 34.419.492L480 342.118V160H292.451a24.005 24.005 0 0 1-16.971-7.029l-16-16C244.361 121.851 255.069 96 276.451 96H520c13.255 0 24 10.745 24 24v222.118l40.416-42.792c9.285-9.831 24.856-10.054 34.419-.492l10.823 10.823c9.372 9.372 9.372 24.569-.001 33.941zm-265.138 15.431A23.999 23.999 0 0 0 347.548 352H160V169.881l40.416 42.792c9.286 9.831 24.856 10.054 34.419.491l10.822-10.822c9.373-9.373 9.373-24.569 0-33.941L144.971 67.716c-9.373-9.373-24.569-9.373-33.941 0L10.343 168.402c-9.373 9.373-9.373 24.569 0 33.941l10.822 10.822c9.562 9.562 25.133 9.34 34.419-.491L96 169.881V392c0 13.255 10.745 24 24 24h243.549c21.382 0 32.09-25.851 16.971-40.971l-16.001-16z")
     #test-container(:style="{fontSize: fontSize+'px', '--space': space, fontWeight: fontWeight}")
-
+      h2#vue Vue
       div(ref="vueSection")
-        h2 Vue
         h3 just for h3+div margin auto style
-        h4 Mode
-        select(v-model="vueMode")
-          option(:value="null") unspecified (dynamic)
-          option(value="accordion") strictly accordion
-          option(value="tabpanel") strictly tabpanel
-        h4 Pick a style
-        select(v-model="vueStyle")
+        label(for="vue-mode") Mode
+        select#vue-mode(v-model="vueMode")
+          option(:value="null") Unspecified (Dynamic)
+          option(value="accordion") Strictly Accordion
+          option(value="tabpanel") Strictly TabPanel
+        label(for="vue-style") Style
+        select#vue-style(v-model="vueStyle")
           option(:value="null") Default
           option(value="fancy") Fancy
           option(value="pills") Pills
           option(value="bar") Bar
-        div(:style="{'opacity': vueStyle === 'bar' ? 0.5 : 1}")
-          input#vue-rounded(type="checkbox" v-model="vueRounded" :disabled="vueStyle === 'bar'")
-          label(for="vue-rounded") Add border-radius
-        div
-          input#vue-centered(type="checkbox" v-model="vueCentered")
-          label(for="vue-centered") Center the tabs
-        div
-          input#vue-rtl(type="checkbox" v-model="vueRtl")
-          label(for="vue-rtl") RTL
-        h4 Tabs
+        fieldset.vue-fieldset
+          div(:style="{'opacity': vueStyle === 'bar' ? 0.5 : 1}")
+            input#vue-rounded(type="checkbox" v-model="vueRounded" :disabled="vueStyle === 'bar'")
+            label(for="vue-rounded") Add border-radius
+          div
+            input#vue-centered(type="checkbox" v-model="vueCentered")
+            label(for="vue-centered") Center the tabs
+          div
+            input#vue-rtl(type="checkbox" v-model="vueRtl")
+            label(for="vue-rtl") RTL
+        h4 Tabs/Headers
         div(v-for="(tab, idx) in vueTabs" :key="idx")
-          input(type="text" v-model="vueTabs[idx]")
+          input.input(type="text" v-model="vueTabs[idx]")
           button(@click="vueTabs.splice(idx, 1)") remove
-        button(@click="vueTabs.push('')") add
-        div(:style="{width: width+'%'}")
+        button.vue-remove(@click="vueTabs.push('')") add
+        div.m-a(:style="{width: width+'%'}")
           div
             //- XXX include html of the vue section below above automatically
             VueTabpanelwidget(:mode="vueMode" :tabs="vueTabs" v-bind="vueProps")
               template(v-slot:panel-0="")
                 p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempus felis id urna vulputate maximus. Aliquam vitae arcu id nulla convallis aliquam. Vivamus at nisl semper, sagittis lectus eu, fringilla nisl.
                 small This #[a(href="#" title="Link used to test keyboard navigation within the widget") link] is here to test keyboard navigation.
-          div
-            pre
-              code {{vueCode}}
-
-
-
+        div
+          pre
+            code {{vueCode}}
       //- because vanilla takes over and replaces widgets... we cannot use vue to update them live (addCentered)
+      h2#vanilla Vanilla
       div(ref="vanillaSection")
-        h2 Vanilla
         fieldset.fieldset
           legend Options
           input#turn-off-tpw.input(type="checkbox" v-model="tpwOff")
