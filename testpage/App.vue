a8a93ae52e735238185cc86b9d33b96ea899f74b
thierryk <thierry.koblentz@gmail.com>
2020-11-23 13:33:24 -0800

    Updated markup and styles

--- testpage/App.vue
+++ testpage/App.vue
@@ -49,6 +49,51 @@
         svg(role="presentation" xmlns="http://www.w3.org/2000/svg" width="50" height="35" viewBox="0 0 640 512")
           path(fill="#fff" d="M629.657 343.598L528.971 444.284c-9.373 9.372-24.568 9.372-33.941 0L394.343 343.598c-9.373-9.373-9.373-24.569 0-33.941l10.823-10.823c9.562-9.562 25.133-9.34 34.419.492L480 342.118V160H292.451a24.005 24.005 0 0 1-16.971-7.029l-16-16C244.361 121.851 255.069 96 276.451 96H520c13.255 0 24 10.745 24 24v222.118l40.416-42.792c9.285-9.831 24.856-10.054 34.419-.492l10.823 10.823c9.372 9.372 9.372 24.569-.001 33.941zm-265.138 15.431A23.999 23.999 0 0 0 347.548 352H160V169.881l40.416 42.792c9.286 9.831 24.856 10.054 34.419.491l10.822-10.822c9.373-9.373 9.373-24.569 0-33.941L144.971 67.716c-9.373-9.373-24.569-9.373-33.941 0L10.343 168.402c-9.373 9.373-9.373 24.569 0 33.941l10.822 10.822c9.562 9.562 25.133 9.34 34.419-.491L96 169.881V392c0 13.255 10.745 24 24 24h243.549c21.382 0 32.09-25.851 16.971-40.971l-16.001-16z")
     #test-container(:style="{fontSize: fontSize+'px', '--space': space, fontWeight: fontWeight}")
+      
+      div(ref="vueSection")
+        h2 Vue
+        h3 just for h3+div margin auto style
+        h4 Mode
+        div
+          input#vue-unspecified(type="radio" v-model="vueMode" :value="undefined")
+          label(for="vue-unspecified")  unspecified (dynamic)
+          input#vue-accordion(type="radio" v-model="vueMode" value="accordion")
+          label(for="vue-accordion") strictly accordion
+          input#vue-tabpanel(type="radio" v-model="vueMode" value="tabpanel")
+          label(for="vue-tabpanel") strictly tabpanel
+        h4 Pick a style
+        select(v-model="vueStyle")
+          option(:value="null") Default
+          option(value="fancy") Fancy
+          option(value="pills") Pills
+          option(value="bar") Bar
+        div(:style="{'opacity': vueStyle === 'bar' ? 0.5 : 1}")
+          input#vue-rounded(type="checkbox" v-model="vueRounded" :disabled="vueStyle === 'bar'")
+          label(for="vue-rounded") Add border-radius
+        div
+          input#vue-centered(type="checkbox" v-model="vueCentered")
+          label(for="vue-centered") Center the tabs
+        div
+          input#vue-rtl(type="checkbox" v-model="vueRtl")
+          label(for="vue-rtl") RTL
+        h4 Tabs
+        div(v-for="(tab, idx) in vueTabs" :key="idx")
+          input(type="text" v-model="vueTabs[idx]")
+          button(@click="vueTabs.splice(idx, 1)") remove
+        button(@click="vueTabs.push('')") add
+        div(:style="{width: width+'%'}")
+          div
+            //- XXX include html of the vue section below above automatically
+            VueTabpanelwidget(:mode="vueMode" :tabs="vueTabs" v-bind="vueProps")
+              template(v-slot:panel-0="")
+                p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempus felis id urna vulputate maximus. Aliquam vitae arcu id nulla convallis aliquam. Vivamus at nisl semper, sagittis lectus eu, fringilla nisl.
+                small This #[a(href="#" title="Link used to test keyboard navigation within the widget") link] is here to test keyboard navigation.
+          div
+            pre
+              code {{vueCode}}  
+      
+      
+      
       //- because vanilla takes over and replaces widgets... we cannot use vue to update them live (addCentered)
       div(ref="vanillaSection")
         h2 Vanilla
@@ -346,41 +391,6 @@
                 code.code.centered tpw-centered
             .tpw-widget.tpw-accordion.tpw-icons-at-the-end.tpw-animate
               include ./_vanilla-headings.pug
-      div(ref="vueSection")
-        h2 Vue
-        h3 just for h3+div margin auto style
-        h4 Mode
-        div
-          label #[input(type="radio" v-model="vueMode" :value="undefined")] unspecified (dynamic)
-          label #[input(type="radio" v-model="vueMode" value="accordion")] strictly accordion
-          label #[input(type="radio" v-model="vueMode" value="tabpanel")] strictly tabpanel
-        h4 Pick a style
-        select(v-model="vueStyle")
-          option(:value="null") Default
-          option(value="fancy") Fancy
-          option(value="pills") Pills
-          option(value="bar") Bar
-        div(:style="{'opacity': vueStyle === 'bar' ? 0.5 : 1}")
-          label #[input(type="checkbox" v-model="vueRounded" :disabled="vueStyle === 'bar'")] Add border-radius
-        div
-          label #[input(type="checkbox" v-model="vueCentered")] Center the tabs
-        div
-          label #[input(type="checkbox" v-model="vueRtl")] RTL
-        h4 Tabs
-        div(v-for="(tab, idx) in vueTabs" :key="idx")
-          input(type="text" v-model="vueTabs[idx]")
-          button(@click="vueTabs.splice(idx, 1)") remove
-        button(@click="vueTabs.push('')") add
-        div(:style="{width: width+'%'}")
-          div
-            //- XXX include html of the vue section below above automatically
-            VueTabpanelwidget(:mode="vueMode" :tabs="vueTabs" v-bind="vueProps")
-              template(v-slot:panel-0="")
-                p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempus felis id urna vulputate maximus. Aliquam vitae arcu id nulla convallis aliquam. Vivamus at nisl semper, sagittis lectus eu, fringilla nisl.
-                small This #[a(href="#" title="Link used to test keyboard navigation within the widget") link] is here to test keyboard navigation.
-          div
-            pre
-              code {{vueCode}}
 </template>
 
 <script>
