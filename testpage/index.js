094354de99bb2d5e70abfea6bcd1afe89703e6d2
TJ Koblentz <tj.koblentz@gmail.com>
2020-12-09 21:40:27 -0800

    ... fix Object.assign, again

--- testpage/index.js
+++ testpage/index.js
@@ -1,4 +1,6 @@
-import 'es6-object-assign' // parcel is incorrectly leaving Object.assign in i believe :(
+import { assign } from 'es6-object-assign' // parcel is incorrectly leaving Object.assign in i believe :(
+if (!Object.assign) Object.assign = assign
+
 import Vue from "vue"
 // import * as vuera from "vuera"
 
