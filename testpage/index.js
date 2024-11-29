054494c69e878e4a94a5d4dc7288cff86e8903e4
TJ Koblentz <tj.koblentz@gmail.com>
2024-11-29 14:12:27 -0800

    start

--- testpage/index.js
+++ testpage/index.js
@@ -1,11 +1,6 @@
-// use require instead of import to order polyfill first
-require('es6-object-assign').polyfill() // parcel is incorrectly leaving Object.assign in i believe :(
-const Vue = require('vue').default
-const App = require('./App.vue').default
+import { createApp } from 'vue'
+import App from './App.vue'
 
 // Vue.use(vuera.VuePlugin)
 
-new Vue({
-  el: '#app',
-  render: h => h(App),
-})
+createApp(App).mount('#app')
